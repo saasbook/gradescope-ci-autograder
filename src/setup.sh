02bbca0811a25db79126e61b427ca12af6500c26
Michael Ball <cycomachead@gmail.com>
2020-11-24 14:34:18 -0800

    tweak installer script

--- src/setup.sh
+++ src/setup.sh
@@ -2,36 +2,31 @@
 set -euv
 
 # Install ruby 2.5, rails stuff, nokogiri stuff.
+# apt-add-repository -y ppa:rael-gc/rvm
 apt-get update
-apt-get install -y sqlite3 libsqlite3-dev nodejs build-essential patch zlib1g-dev liblzma-dev
+apt-get install -y sqlite3 libsqlite3-dev nodejs build-essential patch zlib1g-dev liblzma-dev autoconf bison libssl-dev libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
 
-# # Basic gem utils, if an assignment doesn't use a Gemfile
-# gem install -N bundler --no-ri --no-rdoc
-# gem update --system --no-ri --no-rdoc -q
+# rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
+# $(rbenv init -)
+# source ~/.bashrc
 
-# mkdir ~/.gnupg
-# chown -R $(whoami) ~/.gnupg/
+# type rbenv
 
-# echo "disable-ipv6" >> ~/.gnupg/dirmngr.conf
+# OK, try the "ubuntu rvm"
+# https://github.com/rvm/ubuntu_rvm
+#  apt-get install software-properties-common
+# apt-get install -y rvm
+# echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
+# This should be what's need to use rvm?
+# /bin/bash --login
+# source ~/.bashrc
+# which rvm
+# rvm requirements
+# rvm install 2.6.5
+# rvm use 2.6.5
 
-# gpg --version
-
-# find ~/.gnupg -type f -exec chmod 600 {} \;
-# find ~/.gnupg -type d -exec chmod 700 {} \;
-
-# gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-
-curl -sSL https://get.rvm.io | bash -s stable
-
-set +o nounset
-source /etc/profile.d/rvm.sh
-source /usr/local/rvm/scripts/rvm
-
-rvm requirements
-rvm install 2.6.5
-rvm use 2.6.5
-
-gem install rails -v 5.2.4.4 --no-ri --no-doc
+# gem install rails -v 5.2.4.4 --no-ri --no-doc
