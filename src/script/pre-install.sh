a38d537ae00ee202feedf2f13bce732a3e6e744b
Michael Ball <cycomachead@gmail.com>
2020-11-24 20:23:13 -0800

    rvm preinstall

--- src/script/pre-install.sh
+++ src/script/pre-install.sh
@@ -1,6 +1,6 @@
 bash -c 'BASH_ENV=/etc/profile exec bash'
-# /bin/bash --login
-# rvm install 2.6.5
-# rvm use 2.6.5
+/bin/bash --login
+rvm install 2.6.5
+rvm use 2.6.5
 
-rbenv install 2.6.5
+# rbenv install 2.6.5
